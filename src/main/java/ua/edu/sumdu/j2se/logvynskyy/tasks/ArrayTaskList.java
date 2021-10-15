80a91a4dccd5bf8dd64c77c8357e40f78bc96a44
Logvynskyy <shimon511bm@gmail.com>
2021-10-15 13:36:21 +0300

    Javadoc to class ArrayTaskList added

--- src/main/java/ua/edu/sumdu/j2se/logvynskyy/tasks/ArrayTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/logvynskyy/tasks/ArrayTaskList.java
@@ -2,19 +2,33 @@ package ua.edu.sumdu.j2se.logvynskyy.tasks;
 
 import java.util.*;
 
+/**
+ * Клас ArrayTaskList, що створює список задач taskList типу Task та має методи, що дозволяють працювати з ним.
+ * @author Logvynskyy
+ * @version 1.0
+ */
 public class ArrayTaskList {
 
     private int size = 10;
     private Task[] taskList = new Task[size];
 
+    /**
+     * Метод додає задачу у кінець списку задач
+     * @param task - об'єкт класу Task, що потрібно додати до масиву taskList
+     */
     public void add(Task task){
-        if(emptySpaceCheck()){
+        if(isLastEmpty()){
             taskList = Arrays.copyOf(taskList, size + 5);
             size += 5;
         }
         taskList[size()] = task;
     }
 
+    /**
+     * Метод видаляє задачу зі списку задач та скорочує його розмір на 1
+     * @param task - об'єкт класу Task, що потрібно видалити з масиву taskList
+     * @return - true, якщо задача task була у списку, інакше повертає false
+     */
     public boolean remove(Task task){
         Task[] result = new Task[taskList.length - 1];
         for(int i = 0; i < taskList.length; i++){
@@ -28,6 +42,10 @@ public class ArrayTaskList {
         return false;
     }
 
+    /**
+     * Метод повертає кількість задач у списку
+     * @return - кількість ненульових об'єктів масиву taskList
+     */
     public int size(){
         int counter = 0;
         for (Task task : taskList){
@@ -37,14 +55,25 @@ public class ArrayTaskList {
         return counter;
     }
 
+    /**
+     * Метод повертає задачу, що стоїть на вказаному місці у списку
+     * @param index - індекс елемента масиву, який потрібно повернути
+     * @return - елемент масиву taskList за вказаним індексом
+     */
     public Task getTask(int index){
         return taskList[index];
     }
 
-    private boolean emptySpaceCheck(){
+    private boolean isLastEmpty(){
         return taskList[size - 1] == null;
     }
 
+    /**
+     * Метод для створення списку задач, які будуть виконані хоч раз у вказаному часовому проміжку
+     * @param from - початкова дата відліку
+     * @param to - кінцева дата відліку
+     * @return - підмножину задач, які заплановані на виконання хоча б раз після часу from і не пізніше ніж to
+     */
     public ArrayTaskList incoming(int from, int to){
         ArrayTaskList plannedTasks = new ArrayTaskList();
         for(int i = 0; i < size(); i++){
