686e0de147a1e881346b7286e1e81b8fb58c3afb
Logvynskyy <shimon511bm@gmail.com>
2021-11-09 18:17:26 +0200

    Started working on Iterator

--- src/main/java/ua/edu/sumdu/j2se/logvynskyy/tasks/AbstractTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/logvynskyy/tasks/AbstractTaskList.java
@@ -1,6 +1,11 @@
 package ua.edu.sumdu.j2se.logvynskyy.tasks;
 
-public abstract class AbstractTaskList {
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+public abstract class AbstractTaskList implements Iterable<Task>{
+    private int modCount = 0;
+
     public abstract void add(Task task);
     public abstract boolean remove(Task task);
     public abstract int size();
@@ -25,5 +30,47 @@ public abstract class AbstractTaskList {
             }
         }
         return plannedTasks;
+    }
+
+    @Override
+    public Iterator<Task> iterator() {
+        modCount++;
+        return new Iter();
+    }
+
+    private class Iter implements Iterator<Task>{
+        int index = 0;
+        int lastUsed = -1;
+        int expectedModCount = modCount;
+
+        @Override
+        public boolean hasNext() {
+            return index == size();
+        }
+
+        @Override
+        public Task next() throws IndexOutOfBoundsException{
+            int i = index;
+            Task next = getTask(i);
+            lastUsed = i;
+            index = i + 1;
+            return next;
+        }
+
+        @Override
+        public void remove() {
+            if (lastUsed < 0) throw new IllegalStateException();
+
+            AbstractTaskList.this.remove(getTask(lastUsed));
+            if (lastUsed < index)
+                index--;
+            lastUsed = -1;
+            expectedModCount = modCount;
+        }
+
+        final void checkForComodification() {
+            if (modCount != expectedModCount)
+                throw new ConcurrentModificationException();
+        }
     }
 }
