b78e29ca108c239bf520127d40e21024de7cd5be
Logvynskyy <shimon511bm@gmail.com>
2021-11-15 13:18:42 +0200

    Some personal tests for equals() added

--- src/main/java/ua/edu/sumdu/j2se/logvynskyy/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/logvynskyy/tasks/Main.java
@@ -4,15 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		LinkedTaskList ls = new LinkedTaskList();
-		ls.add(new Task("dsad", 10));
-		ls.add(new Task("dsad", 10));
-		ls.add(new Task("dsad", 10));
-		System.out.println(ls);
 		ArrayTaskList al = new ArrayTaskList();
 		al.add(new Task("dsad", 10));
 		al.add(new Task("dsad", 10));
 		al.add(new Task("dsad", 10));
+		ArrayTaskList al1 = new ArrayTaskList();
+		al1.add(new Task("dsad", 10));
+		al1.add(new Task("dsad", 10));
+		al1.add(new Task("dsad", 10));
 		System.out.println(al);
+		System.out.println(al1);
+		System.out.println(al.equals(al1));
+		al.remove(al.getTask(0));
+		al1.remove(al1.getTask(0));
+		System.out.println(al.equals(al1));
+		al.add(new Task("dsad", 10));
+		al1.add(new Task("dsad", 10));
+		System.out.println(al.equals(al1));
 	}
 }
