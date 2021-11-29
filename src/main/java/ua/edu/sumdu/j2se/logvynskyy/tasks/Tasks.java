d90ad7654d4c0f52e5f34f4c3857bc25187d6cb6
Logvynskyy <shimon511bm@gmail.com>
2021-11-29 02:32:14 +0200

    Method calendar() improved

--- src/main/java/ua/edu/sumdu/j2se/logvynskyy/tasks/Tasks.java
+++ src/main/java/ua/edu/sumdu/j2se/logvynskyy/tasks/Tasks.java
@@ -11,10 +11,14 @@ public class Tasks {
         if(end.isBefore(start)) throw new IllegalArgumentException("Невірні параметри часу!");
 
         AbstractTaskList plannedTasks = TaskListFactory.createTaskList(((AbstractTaskList) tasks).getInstance());
-        ((AbstractTaskList) tasks).getStream()
-                .distinct()
-                .filter(task -> task.nextTimeAfter(start) != null && task.nextTimeAfter(start).isBefore(end))
-                .forEach(plannedTasks::add);
+//        ((AbstractTaskList) tasks).getStream()
+//                .filter(task -> task.nextTimeAfter(start) != null && task.nextTimeAfter(start).isBefore(end))
+//                .forEach(plannedTasks::add);
+        for(Task task : tasks) {
+            if(task.nextTimeAfter(start) != null && task.nextTimeAfter(start).isBefore(end)) {
+                plannedTasks.add(task);
+            }
+        }
         return plannedTasks;
     }
 
@@ -24,21 +28,30 @@ public class Tasks {
         if(end.isBefore(start)) throw new IllegalArgumentException("Невірні параметри часу!");
 
         SortedMap<LocalDateTime, Set<Task>> calendar = new TreeMap<>();
-        Iterable<Task> incomingTasks = incoming(tasks, start, end);
+
+        Iterable<Task> incomingTasks = () -> StreamSupport.stream(tasks.spliterator(), false)
+                .distinct()
+                .filter(t -> t.nextTimeAfter(start) != null && (t.nextTimeAfter(start).isBefore(end)))
+                .iterator();
 
         for (Task incomingTask : incomingTasks) {
-            LocalDateTime nextTaskDate = incomingTask.getTime();
+            LocalDateTime nextTaskDate = incomingTask.nextTimeAfter(start);
             Set<Task> taskSet = new HashSet<>();
             for (Task task : incomingTasks) {
-                if (task.nextTimeAfter(nextTaskDate).isBefore(end)) {
+                if(task.nextTimeAfter(start).isEqual(nextTaskDate)) {
                     taskSet.add(task);
                 }
             }
             calendar.put(nextTaskDate, taskSet);
+//            LocalDateTime nextTaskDate = incomingTask.nextTimeAfter(start);
+//            while (!nextTaskDate.isAfter(end)) {
+//                if ((calendar.containsKey(nextTaskDate)))
+//                    calendar.get(nextTaskDate).add(incomingTask);
+//                else
+//                    calendar.put(nextTaskDate, new HashSet<>(Collections.singleton(incomingTask)));
+//                nextTaskDate = incomingTask.nextTimeAfter(nextTaskDate);
+//            }
         }
-
-
-
         return calendar;
     }
 }
