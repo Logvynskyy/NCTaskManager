36047010f37b36d349f79e11ec1b994e65150a3b
Logvynskyy <shimon511bm@gmail.com>
2021-12-06 18:33:31 +0200

    1st part of JSON IO is ready

--- src/main/java/ua/edu/sumdu/j2se/logvynskyy/tasks/TaskIO.java
+++ src/main/java/ua/edu/sumdu/j2se/logvynskyy/tasks/TaskIO.java
@@ -1,7 +1,11 @@
 package ua.edu.sumdu.j2se.logvynskyy.tasks;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.*;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 public class TaskIO {
     public static void write(AbstractTaskList tasks, OutputStream out){
@@ -63,5 +67,51 @@ public class TaskIO {
         catch(Exception ex){
             ex.printStackTrace();
         }
+    }
+
+    public static void write(AbstractTaskList tasks, Writer out){
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setPrettyPrinting();
+        Gson gson = gsonBuilder.create();
+
+        try(BufferedWriter writer = new BufferedWriter(out)){
+            writer.write(gson.toJson(tasks.getStream()
+                    .toArray(Task[]::new)));
+//            writer.write("{\"tasksArray\":[");
+//            for(int i = 0; i < tasks.size(); i++){
+//                writer.write(gson.toJson(tasks.getTask(i)));
+//                if(i != tasks.size() - 1)
+//                    writer.write(",");
+//            }
+//            writer.write("]}");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void read(AbstractTaskList tasks, Reader in){
+        Gson gson = new Gson();
+        try(BufferedReader reader = new BufferedReader(in)){
+//            int size = gson.fromJson(reader, int.class);
+//            for(int i = 0; i < size; i++){
+//                Task task = gson.fromJson(in, Task.class);
+//                tasks.add(task);
+//            }
+//            reader.read();
+            Task[] taskArray;
+            taskArray = gson.fromJson(reader, Task[].class);
+            Arrays.stream(taskArray).forEach(tasks::add);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeText(AbstractTaskList tasks, File file){
+
+    }
+
+    public static void readText(AbstractTaskList tasks, File file){
+
     }
 }
